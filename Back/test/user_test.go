7d0f8603d85da884c7e5c5463fee5e5b37abc310
Carlos Fontán <fgjcarlos@gmail.com>
2022-03-09 11:35:04 +0100

    Changing the test files to a user_test file

---
+++ Back/test/user_test.go
@@ -0,0 +1,85 @@
+package test
+
+import (
+	"apiBack/controllers/delete"
+	"apiBack/controllers/get"
+	"apiBack/controllers/post"
+	"apiBack/controllers/update"
+
+	u "apiBack/db/models"
+	"testing"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var userID string
+
+func TestCreate(t *testing.T) {
+
+	oid := primitive.NewObjectID()
+	userID = oid.Hex()
+
+	user := u.User{
+		ID:        oid,
+		Name:      "Juan",
+		Email:     "fgjdf@micorrep.com",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err := post.AddUser(user)
+
+	if err != nil {
+		t.Error("Add user test failed")
+		t.Fail()
+	} else {
+		t.Log("The test finished successfully")
+	}
+}
+
+func TestGetUserById(t *testing.T) {
+
+	user, err := get.GetUserById(userID)
+
+	if err != nil {
+		t.Error("Get user buy id test failed")
+		t.Fail()
+	} else {
+		t.Log("The user recived id:", user)
+	}
+
+}
+
+func TestUpdateUser(t *testing.T) {
+
+	user := u.User{
+		Name:      "Manolo",
+		Email:     "nolo@micorrep.com",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err := update.UpdateUser(user, userID)
+
+	if err != nil {
+		t.Error("Update user test failed")
+		t.Fail()
+	} else {
+		t.Log("The test finished successfully")
+	}
+
+}
+
+func TestDeleteUser(t *testing.T) {
+
+	err := delete.DeleteUser(userID)
+
+	if err != nil {
+		t.Error("Delete user test failed")
+		t.Fail()
+	} else {
+		t.Log("The test finished successfully")
+	}
+
+}
