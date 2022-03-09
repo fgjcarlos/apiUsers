876d0d0bc9cfd24737685e2326c69df9a2dd7a2f
Carlos Fontán <fgjcarlos@gmail.com>
2022-03-09 13:56:00 +0100

    initial commit front, and add endpoint single up

--- Back/index.go
+++ Back/index.go
@@ -4,6 +4,8 @@ import (
 	"apiBack/controllers/get"
 	u "apiBack/utils"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -11,19 +13,28 @@ import (
 
 func main() {
 
-	u.LoadEnv()
-
 	// Load var env
+	u.LoadEnv()
 	var PORT = os.Getenv("PORT")
 	var HOST = os.Getenv("HOST")
-	// var MONGO_COLLECTION = os.Getenv("MONGO_DATABASE")
-
-	// Init db with 'users' collections
-	// dbMongo.GetCollection(MONGO_COLLECTION)
 
 	r := gin.Default()
 
 	r.SetTrustedProxies(nil)
+
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+	r.POST("/upload-avatar", func(c *gin.Context) {
+		// Archivo individual
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		file.Filename = "firstNameAvatar"
+
+		// Guarda el archivo recibido a un destino específico
+		c.SaveUploadedFile(file, "/media/avatars")
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
